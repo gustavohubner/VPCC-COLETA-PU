17fd1b4dd695e68d2c2653f2492df1f481afc49d
gustavohubner <gustavohubner98@yahoo.com.br>
2025-03-27 14:09:21 -0300

    vpcc

---
+++ cria_pastas.sh
@@ -0,0 +1,8 @@
+mkdir out/
+mkdir out/logs/
+mkdir out/ply/
+mkdir out/bin/
+mkdir out/idle/
+
+mkdir out/ply/redandblack/ out/ply/longdress/ out/ply/soldier/ out/ply/queen/ out/ply/loot/
+mkdir out/bin/redandblack/ out/bin/longdress/ out/bin/soldier/ out/bin/queen/ out/bin/loot/
---
+++ zond.sh
@@ -0,0 +1,1 @@
+/home/gustavo/Downloads/solveigmm/zond265-x64-demo/zondlauncher /home/gustavo/Documentos/VPCC-HEVC-PUs-COLETA/out/bin/longdress/longdress_r1_HM-Base_RA_GOF0_attribute.bin
